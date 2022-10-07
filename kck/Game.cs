285880fdb427cdd44e7ae08c9b29bb3a722a22a4
Kuba Zastocki <kubazastocki@gmail.com>
2022-10-07 21:47:45 +0200

    Dodalem gre

---
+++ kck/Game.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kck
+{
+    internal class Game
+    {
+       
+        public void Start()
+        {
+            Console.Title = "Wordle";
+            RunMainMenu();
+        }
+
+        private void RunMainMenu()
+        {
+            string prompt = "Witaj w grze!";
+            string[] options = { "Nowa gra", "Zmien liczbe liter", "Stworz wlasne slowo", "Koniec gry" };
+            Menu mainMenu = new Menu(prompt, options);
+            //mainMenu.Display();
+            int selectedIndex = mainMenu.Run();
+            switch (selectedIndex)
+            {
+                case 0:
+                    NewGame();
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+            
+        }
+        private void NewGame()
+        {
+            string test = "test";
+            string word;
+            Console.WriteLine("Podaj słowo:");
+            do
+            {
+                
+                word = Console.ReadLine();
+                if(word != test)
+                {
+                    Console.WriteLine("Nie zgadles~! proboj dalej");
+                }
+            }
+            while (word != test);
+            Console.WriteLine("Zgadles");
+                
+            
+        }
+    }
+}
---
+++ kck/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Data;
+using kck;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Game myGame = new Game();
+        myGame.Start();
+        
+    }
+}
