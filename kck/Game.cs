9e3694a2a42cb65fbd6cfe3e34d505b24d2bcbf8
Kuba Zastocki <kubazastocki@gmail.com>
2022-10-09 16:07:22 +0200

    guessing word works

--- kck/Game.cs
+++ kck/Game.cs
@@ -1,8 +1,10 @@
-﻿namespace kck
+﻿using System.Runtime.CompilerServices;
+
+namespace kck
 {
     internal class Game
     {
-       
+
         public void Start()
         {
             Console.Title = "Wordle";
@@ -14,13 +16,13 @@
             var prompt = "Witaj w grze!";
             var optionsList = new List<string>()
             {
-                "Nowa gra", 
-                "Zmien liczbe liter", 
-                "Stworz wlasne slowo", 
+                "Nowa gra",
+                "Zmien liczbe liter",
+                "Stworz wlasne slowo",
                 "Koniec gry"
             };
             var mainMenu = new Menu(prompt, optionsList);
-            
+
             var selectedIndex = mainMenu.Run();
             switch (selectedIndex)
             {
@@ -37,35 +39,89 @@
                     Console.WriteLine("Błędny wybór");
                     break;
             }
-            
+
         }
         private void NewGame()
         {
-
-            var test = "test";
+            Console.Clear();
+            var test = "testy";
             var word = default(string);
             Console.WriteLine("Podaj słowo:");
             do
             {
-                
                 word = Console.ReadLine();
-                if(word != test)
+                var result = CheckLetters(word, test);
+                DisplayResult(result, word);
+            }
+            while (test!=word);
+
+            //for(int i = 0; i < test.Length; i++)
+            //Console.WriteLine("z tej funkcji wyszlo:" + costam[i]);
+
+        }
+        private void changeColorYellow()
+        {
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+        private void changeColorGreen()
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+        private void changeColorDefault()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public enum LetterStatus
+        {
+                Empty,
+                Wrong,
+                WrongPosition,
+                Correct
+        }
+        public LetterStatus[] CheckLetters(string _word, string _test)
+        {
+            var result = new LetterStatus[_test.Length];
+            for(int i = 0; i < _test.Length; i++)
+            {
+                if (_word[i]== _test[i])
                 {
-                    Console.BackgroundColor = ConsoleColor.Yellow;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.Write(word[0]);
-                    Console.BackgroundColor = ConsoleColor.Green;
-                    Console.Write(word[1]);
-                    
-                    Console.BackgroundColor = ConsoleColor.Black;
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine("\nNie zgadles~! proboj dalej");
+                    result[i] = LetterStatus.Correct;
+                }
+                else if (_test.Contains(_word[i])){
+                    result[i] = LetterStatus.WrongPosition;
+                }
+                else
+                {
+                    result[i] = LetterStatus.Wrong;
                 }
             }
-            while (word != test);
-            Console.WriteLine("Zgadles");
-                
-            
+            return result;
+        }
+        public void DisplayResult(LetterStatus[] _result, string word)
+        {
+            for(int i = 0; i < _result.Length; i++)
+            {
+                if (_result[i] == LetterStatus.Correct)
+                {
+                    changeColorGreen();
+                    Console.Write(word[i]);
+                }
+                else if (_result[i] == LetterStatus.WrongPosition)
+                {
+                    changeColorYellow();
+                    Console.Write(word[i]);
+                }
+                else if (_result[i]== LetterStatus.Wrong)
+                {
+                    changeColorDefault();
+                    Console.Write(word[i]);
+                }
+            }
+            Console.WriteLine();
+            changeColorDefault();
         }
     }
 }
