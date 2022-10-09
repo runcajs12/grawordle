e3e630decadcb789648648ef28b86f86f248242f
Michał Zastocki <kontakt@michalzastocki.pl>
2022-10-09 18:10:53 +0200

    class word.cs

---
+++ kck/Word.cs
@@ -0,0 +1,30 @@
+﻿using System.Data;
+using System.Diagnostics.Tracing;
+using System.Security.Cryptography;
+
+namespace kck
+{
+    public class Word
+    {
+        public List<string> Words { get; set; }
+        public string FileName { get; set; } = "words.txt";
+
+        public Word()
+        {
+            Words = new List<string>();
+
+            if (File.Exists(FileName))
+            {
+                var fileLines = File.ReadAllLines(FileName);
+
+                foreach (var line in fileLines)
+                {
+                    Words.Add(line);
+                }
+            }
+        }
+
+
+
+    }
+}
